0f274520186dcea575926d030d106a614f1cd781
Matthew McDonald <matthew.mcd1031@gmail.com>
2017-10-27 13:24:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase ()
 }
-function logshout(string) {
+function logShout(string) {
   console.log(string.toUpperCase)
 }
-function logwhisper(string) {
+function logWhisper(string) {
   console.log(string.toLowerCase)
 }
